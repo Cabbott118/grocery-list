5ef08c2208d4a93dd4b9bf10ea3900fef07aa514
Caleb Abbott <54942782+Cabbott118@users.noreply.github.com>
2021-05-04 11:41:32 -0400

    Routes

---
+++ functions/api/groceries.js
@@ -0,0 +1,68 @@
+const { db } = require('../util/admin');
+const config = require('../util/config');
+const firebase = require('firebase');
+firebase.initializeApp(config);
+
+// GET
+exports.getGroceries = (request, response) => {
+  db.collection('groceries')
+    .get()
+    .then((data) => {
+      let groceries = [];
+      data.forEach((doc) => {
+        groceries.push({
+          groceryId: doc.id,
+          name: doc.data().name,
+        });
+      });
+      return response.json(groceries);
+    })
+    .catch((error) => {
+      console.error(error);
+      return response.status(500).json({ error: error.code });
+    });
+};
+
+// POST
+exports.postGrocery = (request, response) => {
+  if (request.body.trim() === '') {
+    return response.status(400).json({ body: 'Please enter something to add' });
+  }
+
+  const newGroceryItem = {
+    name: request.body.name,
+    createdAt: new Date().toISOString(),
+  };
+
+  db.collection('groceries')
+    .add(newGroceryItem)
+    .then((data) => {
+      const resNewGroceryItem = newGroceryItem;
+      resNewGroceryItem.groceryId = data.id;
+      response.json(resNewGroceryItem);
+    })
+    .catch((error) => {
+      console.error(error);
+      return response.status(500).json({ error: error.code });
+    });
+};
+
+// DELETE
+exports.deleteGrocery = (req, res) => {
+  const document = db.doc(`/groceries/${req.params.postId}`);
+  document
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Item not found' });
+      }
+      return document.delete();
+    })
+    .then(() => {
+      res.json({ message: 'Item deleted successfully' });
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
--- functions/index.js
+++ functions/index.js
@@ -4,4 +4,11 @@ const app = require('express')();
 const cors = require('cors');
 app.use(cors());
 
+const { getGroceries, postGrocery, deleteGrocery } = require('./api/groceries');
+
+// Grocery Routes
+app.get('/groceries', getGroceries);
+app.post('/postGrocery', postGrocery);
+app.delete('/grocery/:groceryId', deleteGrocery);
+
 exports.api = functions.https.onRequest(app);
